7e5927a3db11e8ca0d648adea956d0535c78ee3f
Prince Ekpenyong <princekpenyong@yahoo.com>
2023-05-30 12:12:26 +0100

    method 1 for fetching data from database

--- src/index.js
+++ src/index.js
@@ -1,43 +1,49 @@
-import React from 'react';
-import ReactDom from 'react-dom/client';
-import './index.css';
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import './index.css'
 
-
+const Books = [
+  {
+    title: 'Book 1 Title',
+    author: 'Author 1',
+    img: './images/pro.jpg',
+    id: 1,
+  },
+  {
+    title: 'Book 2 Title',
+    author: 'Author 2',
+    img: './images/pro.jpg',
+    id: 2,
+  },
+  {
+    title: 'Book 3 Title',
+    author: 'Author 3',
+    img: './images/pro.jpg',
+    id: 3,
+  },
+  {
+    title: 'Book 4 Title',
+    author: 'Author 4',
+    img: './images/pro.jpg',
+    id: 4,
+  },
+]
 const BookList = () => {
   return (
-    <section className='booklist'>
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+    <section className="booklist">
+      {Books.map((bookItem) => {
+        const {img, title, author, id} = bookItem
+        return (
+          <div className="book" key= {id}>
+            <img src={img} />
+            <h2>{title}</h2>
+            <h4>{author}</h4>
+          </div>
+        )
+      })}
     </section>
   )
 }
 
-const Book = () => {
-  return (
-    <article className='book'>
-        <Image />
-        <Title />
-        <Author />
-    </article>
-    
-
-  )
-}
-
-const Image = () => {
-    return <img src="./images/pro.jpg" alt="" />
-}
-
-const Title = () => {
-    return <h2>Book Title</h2>
-}
-
-const Author = () => {
-    return <h4>Author</h4>
-}
-
-
-const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<BookList />);
+const root = ReactDom.createRoot(document.getElementById('root'))
+root.render(<BookList />)
